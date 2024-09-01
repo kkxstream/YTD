780fda2dfa99833a2284afd0040cf8fe2f7ae6dc
KK Xstream <156395658+kkxstream@users.noreply.github.com>
2024-09-01 23:06:36 +0530

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,77 @@
+import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
+import * as ytdl from "https://deno.land/x/ytdl_core@v0.1.2/mod.ts";
+
+const handler = async function(req: Request){
+
+var url=new URL(req.url);
+console.log(url.pathname)
+
+if(url.pathname.indexOf("/vid") > -1) {
+let id=url.searchParams.get("id");
+if(ytdl.validateID(id)){
+
+let yt =await ytdl.getInfo(id); 
+
+return new Response(`{"video":${JSON.stringify(yt.player_response.videoDetails)},"stream":${JSON.stringify(yt.player_response.streamingData)},"captions":${JSON.stringify(yt.player_response.captions) || "\"none\""}}`, { 
+status: 200,
+headers: {
+"content-type": "application/json; charset=utf-8",
+"Access-Control-Allow-Origin":"*",
+} 
+});
+}else{
+return new Response(`{"error":true}`, { 
+status: 404,
+headers: {
+"Content-Type": "application/json; charset=utf-8",
+"Access-Control-Allow-Origin":"*",
+} 
+});
+}  
+}
+else if(url.pathname.indexOf("audio") > -1 ){
+let id=url.searchParams.get("id");
+if(ytdl.validateID(id)){
+
+let yt =await ytdl.getInfo(id); 
+let audioFormats = ytdl.filterFormats(yt.formats, 'audioonly');
+return new Response(`{"audio":${JSON.stringify(audioFormats)}}`, { 
+status: 200,
+headers: {
+"content-type": "application/json; charset=utf-8",
+"Access-Control-Allow-Origin":"*",
+} 
+});
+}else{
+return new Response(`{"error":true}`, { 
+status: 404,
+headers: {
+"Content-Type": "application/json; charset=utf-8",
+"Access-Control-Allow-Origin":"*",
+} 
+});
+}  
+
+} 
+else if(url.pathname=="/"){
+let index=await Deno.readTextFile("./index.html");
+  
+const body = new TextEncoder().encode(index);
+return new Response(body, { 
+status: 200,
+headers: {
+"content-type": "text/html; charset=utf-8",
+} 
+});
+}else{
+return new Response(`<h1>404 NOT FOUND BRUH 🤣🤣🤣🤣`, { 
+status: 404,
+headers: {
+"content-type": "text/html; charset=utf-8",
+} 
+});
+}
+  
+};
+
+serve(handler);
